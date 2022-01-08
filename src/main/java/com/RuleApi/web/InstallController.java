357335dfd9b781c226ff38b13bfcf5ef8263f18b
buxia97 <buxia97@126.com>
2022-01-08 21:12:17 +0800

    2022/01/08累积更新

--- src/main/java/com/RuleApi/web/InstallController.java
+++ src/main/java/com/RuleApi/web/InstallController.java
@@ -1,6 +1,9 @@
 package com.RuleApi.web;
 
+import com.RuleApi.common.RedisHelp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,40 +20,62 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class InstallController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Value("${mybatis.configuration.variables.prefix}")
+    private String prefix;
+
+    RedisHelp redisHelp =new RedisHelp();
     /***
      * 文章删除
      */
     @RequestMapping(value = "/newInstall")
     @ResponseBody
     public String newInstall() {
+
+        String isInstall = redisHelp.getRedis("isInstall",redisTemplate);
+        if(isInstall!=null){
+            return "虽然重复执行也没关系，但是还是尽量不要频繁点哦，十分钟后再来操作吧！";
+        }
+        Integer i = 1;
+        //判断typecho是否安装，或者数据表前缀是否正确
+
         String text = "执行信息 ------";
+        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = '"+prefix+"_users';", Integer.class);
+        if (i == 0){
+            return "Typecho未安装或者数据表前缀不正确，请尝试安装typecho或者修改properties配置文件。";
+        }else{
+            text+="Typecho程序确认安装。";
+        }
         //查询文章表是否存在views字段
-        Integer i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = 'typecho_contents' and column_name = 'views';", Integer.class);
+        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = '"+prefix+"_contents' and column_name = 'views';", Integer.class);
         if (i == 0){
             //新增字段
-            jdbcTemplate.execute("alter table typecho_contents ADD views integer(10) DEFAULT 0;");
+            jdbcTemplate.execute("alter table "+prefix+"_contents ADD views integer(10) DEFAULT 0;");
             text+="数据表typecho_contents，字段views添加完成。";
         }else{
             text+="数据表typecho_contents，字段views已经存在，无需添加。";
         }
         //查询文章表是否存在likes字段
-        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = 'typecho_contents' and column_name = 'likes';", Integer.class);
+        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = '"+prefix+"_contents' and column_name = 'likes';", Integer.class);
         if (i == 0){
-            jdbcTemplate.execute("alter table typecho_contents ADD likes integer(10) DEFAULT 0;");
+            jdbcTemplate.execute("alter table "+prefix+"_contents ADD likes integer(10) DEFAULT 0;");
             text+="数据表typecho_contents，字段likes添加完成。";
         }else{
             text+="数据表typecho_contents，字段likes已经存在，无需添加。";
         }
         //查询用户表是否存在introduce字段
-        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = 'typecho_users' and column_name = 'introduce';", Integer.class);
+        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = '"+prefix+"_users' and column_name = 'introduce';", Integer.class);
         if (i == 0){
-            jdbcTemplate.execute("alter table typecho_users ADD introduce varchar(255);");
+            jdbcTemplate.execute("alter table "+prefix+"_users ADD introduce varchar(255);");
             text+="数据表typecho_users，字段introduce添加完成。";
         }else{
             text+="数据表typecho_users，字段introduce已经存在，无需添加。";
         }
         //查询用户表是否存在account字段
-        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = 'typecho_users' and column_name = 'assets';", Integer.class);
+        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = '"+prefix+"_users' and column_name = 'assets';", Integer.class);
         if (i == 0){
             jdbcTemplate.execute("alter table typecho_users ADD assets integer(11) DEFAULT 0;");
             text+="数据表typecho_users，字段assets添加完成。";
@@ -58,9 +83,9 @@ public class InstallController {
             text+="数据表typecho_users，字段assets已经存在，无需添加。";
         }
         //判断用户日志表是否存在
-        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = 'typecho_userlog';", Integer.class);
+        i = jdbcTemplate.queryForObject("select count(*) from information_schema.columns where table_name = '"+prefix+"_userlog';", Integer.class);
         if (i == 0){
-            jdbcTemplate.execute("CREATE TABLE `typecho_userlog` (" +
+            jdbcTemplate.execute("CREATE TABLE `"+prefix+"_userlog` (" +
                     "  `id` int(11) NOT NULL AUTO_INCREMENT," +
                     "  `uid` int(11) NOT NULL DEFAULT '-1' COMMENT '用户id'," +
                     "  `cid` int(11) NOT NULL DEFAULT '0'," +
@@ -73,10 +98,9 @@ public class InstallController {
         }else{
             text+="数据表typecho_userlog已经存在，无需添加。";
         }
-        if (i == 0){
-            text+="没有可执行的操作";
-        }
         text+=" ------ 执行结束，安装执行完成";
+
+        redisHelp.setRedis("isInstall","1",600,redisTemplate);
         return text;
     }
 }
