8a369c17193a1b8aa5ace9fe259c245e28140152
buxia97 <buxia97@126.com>
2022-12-11 21:37:39 +0800

    添加轮播设置方法

--- src/main/java/com/RuleApi/web/TypechoContentsController.java
+++ src/main/java/com/RuleApi/web/TypechoContentsController.java
@@ -940,6 +940,43 @@ public class TypechoContentsController {
             return Result.getResultJson(0,"操作失败",null);
         }
     }
+    /***
+     * 文章轮播
+     */
+    @RequestMapping(value = "/addSwiper")
+    @ResponseBody
+    public String addSwiper(@RequestParam(value = "key", required = false) String  key,@RequestParam(value = "isswiper", required = false) Integer  isswiper, @RequestParam(value = "token", required = false) String  token) {
+        try {
+            Integer uStatus = UStatus.getStatus(token,this.dataprefix,redisTemplate);
+            if(uStatus==0){
+                return Result.getResultJson(0,"用户未登录或Token验证失败",null);
+            }
+
+            Map map =redisHelp.getMapValue(this.dataprefix+"_"+"userInfo"+token,redisTemplate);
+            String group = map.get("group").toString();
+            if(!group.equals("administrator")){
+                return Result.getResultJson(0,"你没有操作权限",null);
+            }
+            Integer logUid =Integer.parseInt(map.get("uid").toString());
+            TypechoContents info = service.selectByKey(key);
+            //生成typecho数据库格式的修改时间戳
+            Long date = System.currentTimeMillis();
+            String time = String.valueOf(date).substring(0,10);
+            Integer modified = Integer.parseInt(time);
+            info.setModified(modified);
+            info.setCid(Integer.parseInt(key));
+            info.setIsswiper(isswiper);
+            Integer rows = service.update(info);
+            editFile.setLog("管理员"+logUid+"请求轮播文章"+key);
+            JSONObject response = new JSONObject();
+            response.put("code" ,rows > 0 ? 1: 0 );
+            response.put("data" , rows);
+            response.put("msg"  , rows > 0 ? "操作成功" : "操作失败");
+            return response.toString();
+        }catch (Exception e){
+            return Result.getResultJson(0,"操作失败",null);
+        }
+    }
     /**
      * 文章是否评论过（用于回复可见）
      * */
